eb3badd4d995bb10532067d048cbbfafdcd50ab7
Bogdan Covrig <bogcov@gmail.com>
2023-11-17 21:13:54 +0100

    feat: Add grid and card components

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
   content: ["./src/**/*.astro"],
   theme: {
-    extend: {},
+    extend: {
+      borderRadius: {
+        "4xl": "2rem",
+        "5xl": "2.5rem",
+        "6xl": "3rem",
+        "7xl": "3.5rem",
+        "8xl": "4rem",
+        "9xl": "4.5rem",
+        "10xl": "5rem",
+      },
+    },
   },
   plugins: [],
 };
